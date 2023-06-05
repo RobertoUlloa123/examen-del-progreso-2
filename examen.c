89733372db1dbad9f36d9e2b21423941b1b9fe3d
Roberto-Ulloa <111545962+RobertoUlloa123@users.noreply.github.com>
2023-06-05 16:13:24 -0500

    definimos las variables e implementamos los arreglos

--- examen.c
+++ examen.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
+#include <time.h>
+#define alumno 23
+#define progreso 3
 int main()
 {
-
+    float promedio;
+    int nota;
+    int i, j;
+    alumno[i][j];
+    int rand();
+    
+    
     return 0;
 }
