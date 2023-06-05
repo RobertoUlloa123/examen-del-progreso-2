8c53be372464fe392758c681c4afdb505ea2148d
Roberto-Ulloa <111545962+RobertoUlloa123@users.noreply.github.com>
2023-06-05 16:40:22 -0500

    solo esta hasta el promedio

--- examen.c
+++ examen.c
@@ -9,6 +9,21 @@ int main()
     int i, j;
     float nota[i][j];
     int rand();
+     for (i = 0; i < alumno; i++)
+    {
+        int suma = 0, may = 0, max = 0;
+        for (j = 0; j < progreso; j++)
+        {
+            notas[i][j] = rand() % 11;
+            printf("Las notas del estudiante %d en el %d es: %d ", i + 1, j + 1, notas[i][j]);
+            printf("\n");
+            suma = notas[i][j] + suma;
+            promedio = suma / 3;
+        }
+         printf("\n");
+         printf("El promedio del estudiante %d es: %d",i+1,promedio);
+         printf("\n");
+    }
     
     
     return 0;
